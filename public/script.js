d7bf8fb19deebe05fcd572f214ac3b632e6d16a9
DESKTOP-08GHM38\82107 <yskim@tmonet.co.kr>
2022-03-10 19:45:37 +0900

    commit

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-function back(event) {
-    console.log(event);
-    event.postMessage('hello', 'http://localhost:4000');
+function back() {
+    window.parent.postMessage('hello', 'http://localhost:4000');
+    const tag = document.createElement('div');
+    tag.innerText = '+';
+    document.body.append(tag);
 }
